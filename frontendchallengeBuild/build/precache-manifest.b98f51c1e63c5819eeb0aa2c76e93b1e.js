63e8b2807ab1bb704f09a53960ba5e334a004392
Bea Oliveira <beaolivei@gmail.com>
2019-05-17 16:46:02 +0200

    with the build version

---
+++ frontendchallengeBuild/build/precache-manifest.b98f51c1e63c5819eeb0aa2c76e93b1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f416fe459606c0098e672703685651d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79293bde8ba1538c9b8f",
+    "url": "/static/css/main.7a57e05c.chunk.css"
+  },
+  {
+    "revision": "625268c730ee3f78fbd7",
+    "url": "/static/js/2.fb74edd9.chunk.js"
+  },
+  {
+    "revision": "79293bde8ba1538c9b8f",
+    "url": "/static/js/main.c8bc9b3f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b3de3354fb04d55f4bd6e7b8b6f18510",
+    "url": "/static/media/formula1.b3de3354.jpg"
+  }
+]);
